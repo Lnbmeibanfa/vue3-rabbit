819beeb0183ac4fd95705a991613247705577c11
Lin Wanqi <a52hertz_2021@qq.com>
2024-10-05 23:42:24 +0800

    scss颜色自动引入及其测试

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,12 @@ onMounted(async () => {
 
 <template>
   <div>这里是APP</div>
+  <div class="test">test scss</div>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
+</style>
