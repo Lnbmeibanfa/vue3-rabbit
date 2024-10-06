f2c103bc55154a3f543b1137894ab35371a77991
Lin Wanqi <a52hertz_2021@qq.com>
2024-10-06 16:44:42 +0800

    实现banner轮播图,组件疯转,新鲜好物,人气推荐页面的搭建

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,18 +2,18 @@
 import HomeBanner from './components/HomeBanner.vue'
 import HomeCategory from './components/HomeCategory.vue'
 import HomeHot from './components/HomeHot.vue'
-import HomeNew from './components/HomeBanner.vue'
-import HomeProduct from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeProduct from './components/HomeProduct.vue'
 </script>
 
 <template>
   <div class="container">
     <HomeCategory />
     <HomeBanner />
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
   </div>
-  <HomeNew />
-  <HomeHot />
-  <HomeProduct />
 </template>
 
 <style></style>
